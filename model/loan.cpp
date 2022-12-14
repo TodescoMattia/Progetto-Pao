a4e096c204f75b1483e2c9f68118e8c901ad1468
silvia <sylvya.stud@gmail.com>
2022-12-14 18:19:36 +0100

    aggiunta Date + metodo isExpired

--- model/loan.cpp
+++ model/loan.cpp
@@ -1,16 +1,19 @@
 #include "loan.h"
 
 // Costruttore
-Loan::Loan(std::string cod, time_t startD, time_t endD, const User *user,
+Loan::Loan(std::string cod, unsigned int d1, unsigned int m1, unsigned int y1,
+           unsigned int d2, unsigned int m2, unsigned int y2, const User *user,
            const Item *ptr)
-    : codLoan(cod), startDate(startD), endDate(endD), ptrUser(user),
+    : codLoan(cod), startDate(d1, m1, y1), endDate(d2, m2, y2), ptrUser(user),
       ptrItem(ptr) {}
 
 // Metodi
 void Loan::print() const {
   std::cout << codLoan;
-  std::cout << startDate;
-  std::cout << endDate;
+  startDate.print();
+  endDate.print();
   ptrUser->print();
   ptrItem->print();
 }
+
+bool Loan::isExpired() const { return endDate.isBeforeToday(); }
