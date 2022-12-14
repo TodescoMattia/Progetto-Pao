31682f6b5a175423a394f8e8a63e88f571feb83d
silvia <sylvya.stud@gmail.com>
2022-12-14 14:15:26 +0100

    dichiarazione item (costruttore, getter, print)

---
+++ item.h
@@ -0,0 +1,29 @@
+#ifndef ITEM
+#define ITEM
+#include <iostream>
+#include <string>
+
+class Item {
+private:
+  std::string id;
+  std::string title;
+  bool isLent;
+
+public:
+  // Costruttore
+  Item(std::string id = "", std::string tit = "", bool state = 0);
+  //~Item();
+
+  // Getter
+  std::string getId() const;
+  std::string getTitle() const;
+  bool getState() const;
+
+  // Metodi
+  virtual void print() const; // metodo virtuale
+  // virtual void create();
+  // virtual void edit();
+  // virtual void delete();
+};
+
+#endif
