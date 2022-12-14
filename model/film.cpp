722737791167d81511bf402056c42db71bae7ac7
silvia <sylvya.stud@gmail.com>
2022-12-14 23:46:48 +0100

    print film

--- model/film.cpp
+++ model/film.cpp
@@ -2,5 +2,11 @@
 
 // Costruttore
 Film::Film(std::string id, std::string tit, bool state, std::string dir,
-           unsigned int dur)
-    : Item(id, tit, state), director(dir), duration(dur) {}
+           unsigned int dur, Genre gen)
+    : Item(id, tit, state), director(dir), duration(dur), filmGenre(gen) {}
+
+// Metodi
+void Film::print() const {
+  Item::print();
+  std::cout << director << duration << filmGenre;
+}
