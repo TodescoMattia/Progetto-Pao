e07dbcb3fdcc3c8cae292dd894a4592bd69914fa
silvia <sylvya.stud@gmail.com>
2022-12-14 23:46:25 +0100

    print bookserie

--- model/bookserie.cpp
+++ model/bookserie.cpp
@@ -2,6 +2,13 @@
 
 // Costruttore
 BookSerie::BookSerie(std::string id, std::string tit, bool state,
-                     std::string aut, unsigned int pag, std::string gen,
+                     std::string aut, unsigned int pag, Genre gen,
                      unsigned int vol)
     : Book(id, tit, state, aut, pag, gen), volumeNumber(vol) {}
+
+// Metodi
+void BookSerie::print() const {
+  Item::print();
+  Book::print();
+  std::cout << volumeNumber;
+}
--- model/bookserie.h
+++ model/bookserie.h
@@ -9,8 +9,11 @@ private:
 public:
   // Costruttore
   BookSerie(std::string id, std::string tit = "", bool state = false,
-            std::string aut = "", unsigned int pag = 0, std::string gen = "",
+            std::string aut = "", unsigned int pag = 0, Genre gen = (Genre)0,
             unsigned int vol = 0);
+
+  // Metodi
+  void print() const;
 };
 
 #endif
