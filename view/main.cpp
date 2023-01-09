a762a27809251b3b2494d3b3db10df436530cddd
silvia <sylvya.stud@gmail.com>
2023-01-09 16:11:25 +0100

    aggiungo main

--- view/main.cpp
+++ view/main.cpp
@@ -1,11 +1,37 @@
-#include "bookviewer.h"
+#include "homeviewer.h"
+#include "mainwindow.h"
+
+#include "itemtab.h"
+#include "usertab.h"
+#include "loantab.h"
+
+#include "..\model\Book.h"
+#include "..\model\Film.h"
+#include "..\model\User.h"
+#include "..\model\BookSerie.h"
+#include "..\model\List.h"
+#include "listview.h"
+#include "userinfo.h"
+
 
 #include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QFile>
+#include <QTabWidget>
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    BookViewer w;
+    QApplication app(argc, argv);
+    //HomeViewer w;
+
+    QFile stylesheet(":/assets/stylesheet.qss");
+    stylesheet.open(QFile::ReadOnly);
+    app.setStyleSheet(QLatin1String(stylesheet.readAll()));
+
+    MainWindow w;
     w.show();
-    return a.exec();
+
+    return app.exec();
 }
