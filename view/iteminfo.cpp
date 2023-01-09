349fce9757a91be24e0f26a7dc97e9080b2c8aea
silvia <sylvya.stud@gmail.com>
2023-01-09 13:12:52 +0100

    creata classe ItemInfo

--- view/iteminfo.cpp
+++ view/iteminfo.cpp
@@ -1,6 +1,32 @@
+#include <QVBoxLayout>
+#include <QString>
 #include "iteminfo.h"
+#include "ItemInfoVisitor.h"
 
-ItemInfo::ItemInfo()
+ItemInfo::ItemInfo(Item& item)
+: item(item)
 {
+    widget= new QWidget();
 
+    QVBoxLayout* itemInfo = new QVBoxLayout(widget);
+    itemInfo->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+
+    idLabel = new QLabel("Id: " + QString::fromStdString(item.getId()));
+    itemInfo->addWidget(idLabel);
+
+    titleLabel = new QLabel("Titolo: " + QString::fromStdString(item.getTitle()));
+    itemInfo->addWidget(titleLabel);
+
+    isLentLabel = new QLabel("Stato: " + QString::fromStdString(toString(item.getState())));
+    itemInfo->addWidget(isLentLabel);
+
+    ItemInfoVisitor visitor;
+    item.accept(visitor);
+    itemInfo->addWidget(visitor.getWidget());
 }
+
+QWidget* ItemInfo::getWidget() const{
+    return widget;
+}
+
+
--- view/iteminfo.h
+++ view/iteminfo.h
@@ -1,11 +1,22 @@
 #ifndef ITEMINFO_H
 #define ITEMINFO_H
 
+#include "..\model\Item.h"
+#include "Info.h"
+#include <QLabel>
+#include <QWidget>
 
-class ItemInfo: public QWidget{
-    Q_OBJECT
+class ItemInfo : public Info {
+private:
+  Item &item;
+  QWidget *widget;
+  QLabel *idLabel;
+  QLabel *titleLabel;
+  QLabel *isLentLabel;
+
 public:
-    ItemInfo(Item& item, QWidget* parent=0);
+  ItemInfo(Item &item);
+  QWidget *getWidget() const;
 };
 
 #endif // ITEMINFO_H
