2120725a68d177222543a0e24edbcf90de7db483
silvia <sylvya.stud@gmail.com>
2023-01-09 13:16:24 +0100

    aggiunta ItemTab

---
+++ view/itemtab.cpp
@@ -0,0 +1,31 @@
+#include "itemtab.h"
+#include "listwidget.h"
+
+#include <QLabel>
+#include <QScrollArea>
+#include <QVBoxLayout>
+
+ItemTab::ItemTab() {
+
+  // Creates widgets
+  QVBoxLayout *vbox = new QVBoxLayout(this);
+  vbox->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+
+  QLabel *titleLabel = new QLabel("Elenco oggetti ");
+  // titleLabel->setObjectName("title");
+  titleLabel->setAlignment(Qt::AlignHCenter);
+  vbox->addWidget(titleLabel);
+
+  ListWidget *itemList = new ListWidget();
+
+  QScrollArea *scrollArea = new QScrollArea();
+  scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  scrollArea->setWidgetResizable(true);
+  scrollArea->setWidget(itemList);
+
+  vbox->addWidget(scrollArea);
+
+  QPushButton *addItem = new QPushButton("Aggiungi oggetto");
+  vbox->addWidget(addItem);
+}
---
+++ view/itemtab.h
@@ -0,0 +1,11 @@
+#ifndef ITEMTAB_H
+#define ITEMTAB_H
+#include <QWidget>
+
+class ItemTab:public QWidget{
+    Q_OBJECT
+public:
+    ItemTab();
+};
+
+#endif // ITEMTAB_H
