1159df85513566ad749a3971e60594cb46740996
silvia <sylvya.stud@gmail.com>
2023-01-14 10:05:23 +0100

    carico connect funzionante item

--- view/itemtab.cpp
+++ view/itemtab.cpp
@@ -1,5 +1,8 @@
 #include "itemtab.h"
 #include "listwidget.h"
+#include "iteminfo.h"
+#include "..\model\book.h"
+#include "additem.h"
 
 #include <QLabel>
 #include <QScrollArea>
@@ -26,8 +29,39 @@ ItemTab::ItemTab() {
 
   vbox->addWidget(scrollArea);
 
-  QPushButton *addItem = new QPushButton("Aggiungi oggetto");
-  vbox->addWidget(addItem);
+  QPushButton *addItemButton = new QPushButton("Aggiungi oggetto");
+  vbox->addWidget(addItemButton);
 
+  connect(addItemButton, &QPushButton::clicked, this, ItemTab::createItem);
+}
+
+
+void ItemTab::createItem(){
+    AddItem* dialog= new AddItem();
+
+    connect(dialog,SIGNAL(accepted()),this,SLOT(confirm()));
+
+    dialog->setModal(true);
+    dialog->show();
+}
+
+void ItemTab::confirm(){
+    AddItem* dialog=dynamic_cast<AddItem*>(sender());
+    if(dialog==nullptr) return;
+
+    QWidget* provo= new QWidget();
+
+    QVBoxLayout* provo3 = new QVBoxLayout(provo);
+    QLabel* provo2= new QLabel("yohohohhoho");
+    provo3->addWidget(provo2);
+
+    provo->show();
+
+    //std::string name = dialog->getName().toStdString();
+   // std::string surname = dialog->getName().toStdString();
+   // std::string number = dialog->getName().toStdString();
+
+    //momentaneo
+    //listUser.push_front(new User(name, surname, number));
 
 }
--- view/itemtab.h
+++ view/itemtab.h
@@ -1,12 +1,18 @@
 #ifndef ITEMTAB_H
 #define ITEMTAB_H
 #include <QWidget>
+#include "..\model\list.h"
+#include "..\model\item.h"
 
 class ItemTab:public QWidget{
     Q_OBJECT
+private:
+    List<Item*> listItem;
 public:
     ItemTab();
-
+public slots:
+    void createItem();
+    void confirm();
 };
 
 #endif // ITEMTAB_H
