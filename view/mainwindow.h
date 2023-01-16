769da511f78d4246a49612a06fd2aa5754ad3c40
silvia <sylvya.stud@gmail.com>
2023-01-16 14:19:29 +0100

    metodo refreshdata aggiunro

--- view/mainwindow.h
+++ view/mainwindow.h
@@ -11,12 +11,17 @@
 #include <QLabel>
 #include <QPushButton>
 #include <Qstring>
+#include <QTabWidget>
+#include "..\model\list.h"
+#include "..\model\item.h"
 
 
 
 class MainWindow: public QWidget{
     Q_OBJECT
 private:
+    List<Item*> listItem;
+    QTabWidget *tabWidget;
     void Schermo(QVBoxLayout* MainLayout);
     void TabDialogViewer (QVBoxLayout* MainLayout);
     void BarraDeiMenu(QVBoxLayout* MainLayout);
@@ -24,6 +29,8 @@ private:
 public:
     explicit MainWindow(QWidget *parent =nullptr);
 
+public slots:
+    MainWindow& refreshData();
 signals:
 };
 
