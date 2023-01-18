8eed122a29a824b0bbdbc7fc8487dd75c8a48458
silvia <sylvya.stud@gmail.com>
2023-01-18 03:51:19 +0100

    edit videogame

---
+++ view/editvideogame.cpp
@@ -0,0 +1,65 @@
+#include "editvideogame.h"
+
+#include <QVBoxLayout>
+#include <QRadioButton>
+
+EditVideogame::EditVideogame(Videogame* videogame): EditItem(videogame), videogame(videogame)
+{
+    QLayout* layout = this->layout();
+
+    QRadioButton* fantasyButton = new QRadioButton("Fantasy");
+    QRadioButton* horrorButton = new QRadioButton("Horror");
+    QRadioButton* romanceButton = new QRadioButton("Romance");
+    QRadioButton* comedyButton = new QRadioButton("Comedy");
+    QRadioButton* thrillerButton = new QRadioButton("Thriller");
+
+    genreGroup = new QButtonGroup();
+        genreGroup->addButton(fantasyButton, 0);
+        genreGroup->addButton(horrorButton, 1);
+        genreGroup->addButton(romanceButton, 2);
+        genreGroup->addButton(comedyButton, 3);
+        genreGroup->addButton(thrillerButton, 4);
+
+    switch(toInt(videogame->getVGameGenre())){
+    case 0:
+        fantasyButton->setChecked(true);
+        break;
+    case 1:
+        horrorButton->setChecked(true);
+        break;
+    case 2:
+        romanceButton->setChecked(true);
+        break;
+    case 3:
+        comedyButton->setChecked(true);
+        break;
+    case 4:
+        thrillerButton->setChecked(true);
+        break;
+    }
+
+    buttonBox = new QDialogButtonBox();
+
+    confirmButton = new QPushButton("Conferma");
+    cancelButton = new QPushButton("Annulla");
+
+    buttonBox->addButton(confirmButton, QDialogButtonBox::AcceptRole);
+    buttonBox->addButton(cancelButton, QDialogButtonBox::RejectRole);
+
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(setVideogame()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+
+    layout->addWidget(fantasyButton);
+    layout->addWidget(horrorButton);
+    layout->addWidget(romanceButton);
+    layout->addWidget(comedyButton);
+    layout->addWidget(thrillerButton);
+    layout->addWidget(buttonBox);
+
+}
+
+void EditVideogame::setVideogame(){
+    this->set();
+    videogame->setVGameGenre(toGenre(genreGroup->checkedId()));
+    accept();
+}
---
+++ view/editvideogame.h
@@ -0,0 +1,29 @@
+#ifndef EDITVIDEOGAME_H
+#define EDITVIDEOGAME_H
+
+#include "../model/videogame.h"
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QButtonGroup>
+
+#include "edititem.h"
+
+class EditVideogame: public EditItem{
+    Q_OBJECT
+private:
+    Videogame* videogame;
+
+    QButtonGroup *genreGroup;
+
+    QDialogButtonBox *buttonBox;
+    QPushButton* confirmButton;
+    QPushButton* cancelButton;
+
+public:
+    EditVideogame(Videogame* videogame);
+
+public slots:
+    void setVideogame();
+};
+
+#endif // EDITVIDEOGAME_H
