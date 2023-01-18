31ba104d38b6d8a930ad1f7f43bee01da56ce345
silvia <sylvya.stud@gmail.com>
2023-01-18 03:54:20 +0100

    aggiornamento itemtab

--- view/itemtab.cpp
+++ view/itemtab.cpp
@@ -1,9 +1,11 @@
 #include "itemtab.h"
-#include "../model/boardgame.h"
+
 #include "../model/book.h"
 #include "../model/bookserie.h"
 #include "../model/film.h"
+#include "../model/genre.h"
 #include "../model/videogame.h"
+#include "../model/boardgame.h"
 #include "additem.h"
 #include "iteminfo.h"
 #include "listwidget.h"
@@ -12,109 +14,121 @@
 #include <QScrollArea>
 #include <QVBoxLayout>
 
-ItemTab::ItemTab(List<Item *> &lista) : listItem(lista) {
+ItemTab::ItemTab(List<Item *>* lista, MainWindow * mainWindow) : listItem(lista), mainWindow(mainWindow) {
 
-  // Creates widgets
-  QVBoxLayout *vbox = new QVBoxLayout(this);
-  vbox->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+    // Creates widgets
+    QVBoxLayout *vbox = new QVBoxLayout(this);
+    vbox->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
 
-  QLabel *titleLabel = new QLabel("Elenco oggetti ");
-  // titleLabel->setObjectName("title");
-  titleLabel->setAlignment(Qt::AlignHCenter);
-  vbox->addWidget(titleLabel);
+    QLabel *titleLabel = new QLabel("Elenco oggetti ");
 
-  List<Info *> listInfo = *(new List<Info *>);
+    titleLabel->setAlignment(Qt::AlignHCenter);
+    vbox->addWidget(titleLabel);
 
-  ListWidget *itemList = new ListWidget(listInfo);
+    //Creo Info
 
-  QScrollArea *scrollArea = new QScrollArea();
-  scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-  scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-  scrollArea->setWidgetResizable(true);
-  scrollArea->setWidget(itemList);
+    List<Item*>::Iterator start = listItem->begin();
+    List<Item*>::Iterator end = listItem->end();
 
-  vbox->addWidget(scrollArea);
+    List<Info*> listInfo = *(new List<Info *>);
 
-  QPushButton *addItemButton = new QPushButton("Aggiungi oggetto");
-  vbox->addWidget(addItemButton);
+    for (start = listItem->begin(), end = listItem->end(); start != end; start++) {
+        Info* info= new ItemInfo(*start, mainWindow);
+        listInfo.push_front(info);
+    }
 
-  connect(addItemButton, &QPushButton::clicked, this, &ItemTab::selectItem);
+    ListWidget *itemList = new ListWidget(listInfo);
+
+    QScrollArea *scrollArea = new QScrollArea();
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setWidget(itemList);
+
+    vbox->addWidget(scrollArea);
+
+    QPushButton *addItemButton = new QPushButton("Aggiungi oggetto");
+    vbox->addWidget(addItemButton);
+
+    connect(addItemButton, &QPushButton::clicked, this, &ItemTab::selectItem);
 }
 
 void ItemTab::selectItem() {
-  itemSelector = new AddItemSelector();
+    itemSelector = new AddItemSelector();
 
-  itemSelector->setModal(true);
-  itemSelector->show();
+    itemSelector->setModal(true);
+    itemSelector->show();
 
-  connect(itemSelector, SIGNAL(accepted()), this, SLOT(createItem()));
+    connect(itemSelector, SIGNAL(accepted()), this, SLOT(createItem()));
 
-  // connect(dialog, &AddItem::refreshItemTab, this, &ItemTab::refreshItemTab);
-}
+    // connect(dialog, &AddItem::refreshItemTab, this, &ItemTab::refreshItemTab);
+    }
 
 void ItemTab::createItem() {
-  AddItem *dialog = new AddItem(itemSelector);
-  connect(dialog, SIGNAL(accepted()), this, SLOT(confirm()));
+    AddItem *dialog = new AddItem(itemSelector);
+    connect(dialog, SIGNAL(accepted()), this, SLOT(confirm()));
 
-  dialog->setModal(true);
-  dialog->show();
+    dialog->setModal(true);
+    dialog->show();
 }
 
 void ItemTab::confirm() {
-  AddItem *dialog = dynamic_cast<AddItem *>(sender());
-  if (dialog == nullptr)
+    AddItem *dialog = dynamic_cast<AddItem *>(sender());
+    if (dialog == nullptr)
     return;
 
-  // momentaneo
+    std::string id = dialog->getId().toStdString();
+    std::string title = dialog->getTitle().toStdString();
+    bool isLent = static_cast<bool>(dialog->getIsLent());
 
-  std::string id = dialog->getId().toStdString();
-  std::string title = dialog->getTitle().toStdString();
-  bool isLent = static_cast<bool>(dialog->getIsLent());
+    switch (dialog->getSelectedType()) {
+    case 0: {
 
-  switch (dialog->getSelectedType()) {
-  case 0: {
-    std::string authorBook = dialog->getAuthor().toStdString();
-    int pageNumberBook = dialog->getPageNumber();
-    Genre genreBook = static_cast<Genre>(dialog->getGenre());
-    listItem.push_front(
-        new Book(id, title, isLent, authorBook, pageNumberBook, genreBook));
-    Book *provo =
-        new Book(id, title, isLent, authorBook, pageNumberBook, genreBook);
-    provo->print();
-    break;
-  }
-  case 1: {
-    std::string authorBookSerie = dialog->getAuthor().toStdString();
-    int pageNumber = dialog->getPageNumber();
-    Genre genreBookSerie = static_cast<Genre>(dialog->getGenre());
-    int volume = dialog->getVolumeNumber();
-    listItem.push_front(new BookSerie(id, title, isLent, authorBookSerie,
-                                      pageNumber, genreBookSerie, volume));
-    break;
-  }
-  case 2: {
-    std::string director = dialog->getDirector().toStdString();
-    int duration = dialog->getDuration();
-    Genre genreFilm = static_cast<Genre>(dialog->getGenre());
-    listItem.push_front(
-        new Film(id, title, isLent, director, duration, genreFilm));
-    break;
-  }
-  case 3: {
-    Genre genreBoardGame = static_cast<Genre>(dialog->getGenre());
-    int playerNumber = dialog->getDuration();
-    listItem.push_front(
-        new BoardGame(id, title, isLent, genreBoardGame, playerNumber));
-    break;
-  }
-  case 4: {
-    Genre genreVideogame = static_cast<Genre>(dialog->getGenre());
-    listItem.push_front(new Videogame(id, title, isLent, genreVideogame));
-    break;
-  }
-  }
+        std::string authorBook = dialog->getAuthor().toStdString();
+        int pageNumberBook = dialog->getPageNumber();
+        Genre genreBook = toGenre(dialog->getGenre());
+        listItem->push_front(new Book(id, title, isLent, authorBook, pageNumberBook, genreBook));
 
-  emit reload();
+        break;
+    }
+    case 1: {
+
+        std::string authorBookSerie = dialog->getAuthor().toStdString();
+        int pageNumber = dialog->getPageNumber();
+        Genre genreBookSerie = toGenre(dialog->getGenre());
+        int volume = dialog->getVolumeNumber();
+        listItem->push_front(new BookSerie(id, title, isLent, authorBookSerie, pageNumber, genreBookSerie, volume));
+
+        break;
+    }
+    case 2: {
+
+        std::string director = dialog->getDirector().toStdString();
+        int duration = dialog->getDuration();
+        Genre genreFilm = toGenre(dialog->getGenre());
+        listItem->push_front(new Film(id, title, isLent, director, duration, genreFilm));
+
+        break;
+    }
+    case 3: {
+
+        Genre genreBoardGame = toGenre(dialog->getGenre());
+        int playerNumber = dialog->getPlayerNumber();
+        listItem->push_front(new BoardGame(id, title, isLent, genreBoardGame, playerNumber));
+
+        break;
+    }
+    case 4: {
+
+        Genre genreVideogame = toGenre(dialog->getGenre());
+        listItem->push_front(new Videogame(id, title, isLent, genreVideogame));
+
+        break;
+    }
+    }
+
+    emit reload();
+
 }
 
 /*void ItemTab::refreshItemTab(){
--- view/itemtab.h
+++ view/itemtab.h
@@ -4,15 +4,17 @@
 #include "../model/list.h"
 #include "../model/item.h"
 #include "additemselector.h"
+#include "mainwindow.h"
 
 class ItemTab:public QWidget{
     Q_OBJECT
 private:
-    List<Item*> listItem;
+    List<Item*>* listItem;
     AddItemSelector *itemSelector;
+    MainWindow * mainWindow;
 
 public:
-    ItemTab(List<Item*>& lista);
+    ItemTab(List<Item*>* lista, MainWindow * mainWindow);
    // ItemTab& reloadData();
 
 signals:
