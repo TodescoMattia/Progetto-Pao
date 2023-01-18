3e12a9745c91b9a5fee77e9a0c7d684d76dcfe2a
silvia <sylvya.stud@gmail.com>
2023-01-18 03:53:20 +0100

    aggiornamento iteminfo

--- view/iteminfo.cpp
+++ view/iteminfo.cpp
@@ -1,33 +1,48 @@
 #include "iteminfo.h"
+#include "edititem.h"
 #include "iteminfovisitor.h"
+#include "itemeditvisitor.h"
 #include <QString>
 #include <QVBoxLayout>
 
-ItemInfo::ItemInfo(Item &item) : item(item) {
-  widget = new QWidget();
+ItemInfo::ItemInfo(Item *item, MainWindow * mainWindow) : item(item), mainWindow(mainWindow) {
+    widget = new QWidget();
 
-  QVBoxLayout *itemInfo = new QVBoxLayout(widget);
-  itemInfo->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    QVBoxLayout *itemInfo = new QVBoxLayout(widget);
+    itemInfo->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 
-  idLabel = new QLabel("Id: " + QString::fromStdString(item.getId()));
-  itemInfo->addWidget(idLabel);
+    idLabel = new QLabel("Id: " + QString::fromStdString(item->getId()));
+    itemInfo->addWidget(idLabel);
 
-  titleLabel = new QLabel("Titolo: " + QString::fromStdString(item.getTitle()));
-  itemInfo->addWidget(titleLabel);
+    titleLabel = new QLabel("Titolo: " + QString::fromStdString(item->getTitle()));
+    itemInfo->addWidget(titleLabel);
 
-  isLentLabel =
-      new QLabel("Stato: " + QString::fromStdString(toString(item.getState())));
-  itemInfo->addWidget(isLentLabel);
+    isLentLabel =
+      new QLabel("Stato: " + QString::fromStdString(toString(item->getState())));
+    itemInfo->addWidget(isLentLabel);
 
-  ItemInfoVisitor visitor;
-  item.accept(visitor);
-  itemInfo->addWidget(visitor.getWidget());
+    ItemInfoVisitor visitor;
+    item->accept(visitor);
+    itemInfo->addWidget(visitor.getWidget());
 }
 
 QWidget *ItemInfo::getWidget() const { return widget; }
 
 void ItemInfo::edit() {
-  QWidget *prova = new QWidget();
-  prova->show();
+
+    ItemEditVisitor visitor(mainWindow);
+    item->accept(visitor);
+
+
 }
-void ItemInfo::remove() {}
+void ItemInfo::remove() {
+    //mainWindow->removeItem();
+
+}
+
+
+void ItemInfo::confirm(){
+
+
+
+}
--- view/iteminfo.h
+++ view/iteminfo.h
@@ -3,22 +3,29 @@
 
 #include "../model/item.h"
 #include "info.h"
+#include "../model/list.h"
+#include "mainwindow.h"
 #include <QLabel>
 #include <QWidget>
 
 class ItemInfo : public Info {
 private:
-  Item &item;
+  Item *item;
+  MainWindow* mainWindow;
+  List<Item*>* listItem;
   QWidget *widget;
   QLabel *idLabel;
   QLabel *titleLabel;
   QLabel *isLentLabel;
 
 public:
-  ItemInfo(Item &item);
+  ItemInfo(Item *item, MainWindow * mainWindow);
   QWidget *getWidget() const;
   void edit();
   void remove();
+
+public slots:
+  void confirm();
 };
 
 #endif // ITEMINFO_H
